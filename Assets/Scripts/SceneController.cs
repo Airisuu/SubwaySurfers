778a574505eb806d011602875c5753528e122a18
Airisuu <138513253+Airisuu@users.noreply.github.com>
2026-01-21 20:31:31 -0600

    Finish Menu, Add Animations

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+   [SerializeField]
+
+   private Animator Fade;
+   [SerializeField]
+
+   private string FadeAnimationName;
+
+   public void GoToSceneWithFade(string sceneName)
+   {
+        StartCoroutine(LoadSceneAfterFade(sceneName));
+   }
+
+   private IEnumerator LoadSceneAfterFade(string sceneName)
+   {
+    Fade.Play(FadeAnimationName, 0, 0f);
+    yield return new WaitForSeconds(Fade.GetCurrentAnimatorStateInfo(0).length);
+    SceneManager.LoadScene(sceneName);
+   }
+
+}
